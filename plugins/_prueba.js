a086d05fc564ef581f43d895f90d69b52eb1081f
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-11-28 05:47:06 -0500

    Actualizar _prueba.js

---
+++ plugins/_prueba.js
@@ -0,0 +1,92 @@
+/*─────────────────────────────────────────
+  GitHub   : https://github.com/El-brayan502 
+  Creator  : 🕸️ ˚₊ Spenzy XD・୧
+  Whatsapp channel : https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i
+──────────────────────────────────────────*/
+
+const _0x3faa7c = _0x55ec;
+(function (_0xb91bac, _0x7d315a) {
+    const _0x196e25 = _0x55ec, _0x169978 = _0xb91bac();
+    while (!![]) {
+        try {
+            const _0x3f71a3 = -parseInt(_0x196e25(0x199)) / (-0x20af * 0x1 + 0x24e1 + -0x431) * (parseInt(_0x196e25(0x1a7)) / (-0x3 * 0x6e2 + 0x2c7 * 0xa + -0x71e)) + -parseInt(_0x196e25(0x1a5)) / (0x119d + -0x17d3 + 0x1b * 0x3b) * (parseInt(_0x196e25(0x19a)) / (0x302 * -0x9 + -0x12f8 * 0x2 + -0xcb * -0x52)) + -parseInt(_0x196e25(0x19b)) / (0x19ce + 0xd54 + -0x271d) + parseInt(_0x196e25(0x1ab)) / (0x1 * -0x41b + 0x134e + -0xf2d) * (-parseInt(_0x196e25(0x1ac)) / (-0xfc1 + 0x1a32 + -0xa6a)) + parseInt(_0x196e25(0x1a6)) / (0x2304 + -0x993 + 0x1969 * -0x1) + -parseInt(_0x196e25(0x1a9)) / (-0x170e + -0xb15 * -0x1 + -0x3a * -0x35) * (parseInt(_0x196e25(0x1a1)) / (-0x1f2a + 0x1 * 0x80f + -0xed * -0x19)) + -parseInt(_0x196e25(0x1a8)) / (-0x203 * 0x8 + -0xbb * -0x3 + 0xdf2) * (-parseInt(_0x196e25(0x1ad)) / (0xbf * -0xd + 0x199c + -0xfdd));
+            if (_0x3f71a3 === _0x7d315a)
+                break;
+            else
+                _0x169978['push'](_0x169978['shift']());
+        } catch (_0x177c5d) {
+            _0x169978['push'](_0x169978['shift']());
+        }
+    }
+}(_0x2336, -0xdad9e + -0x9c71f + 0x1 * 0x211971));
+function _0x2336() {
+    const _0xfcbd54 = [
+        'msg',
+        'updateProfilePicture',
+        'quoted',
+        '449pGQQwb',
+        '4361304EhCAbF',
+        '1857905QfIYAt',
+        'reply',
+        'mimetype',
+        'propietario',
+        'jid',
+        'owner',
+        '757310YjAOOD',
+        'Error:\x20',
+        'chabc',
+        'RDtse',
+        '3rOeABw',
+        '3800904ugQhQm',
+        '3730kZWHpS',
+        '44yydwfI',
+        '144BcHhJh',
+        'download',
+        '7170846lXXtdo',
+        '7piJNYn',
+        '14589012uFLPjN',
+        'setppf',
+        'message',
+        'help',
+        'CQccN',
+        'error',
+        'logger'
+    ];
+    _0x2336 = function () {
+        return _0xfcbd54;
+    };
+    return _0x2336();
+}
+let handler = async (_0x41a28d, {
+    conn: _0x126c88,
+    usedPrefix: _0x57b552,
+    command: _0x407f56
+}) => {
+    const _0x591ae6 = _0x55ec, _0x19c1c0 = {
+            'CQccN': function (_0x5d3baa, _0x43befa) {
+                return _0x5d3baa + _0x43befa;
+            },
+            'RDtse': 'No\x20se\x20pudo\x20descargar\x20la\x20imagen.',
+            'chabc': 'La\x20imagen\x20del\x20perfil\x20del\x20bot\x20se\x20actualizó\x20correctamente.'
+        }, _0x539f77 = _0x126c88['user'][_0x591ae6(0x19f)], _0x3b8435 = _0x41a28d['quoted'] ? _0x41a28d[_0x591ae6(0x198)] : _0x41a28d, _0x3087a9 = (_0x3b8435[_0x591ae6(0x196)] || _0x3b8435)[_0x591ae6(0x19d)] || '';
+    if (!/image/['test'](_0x3087a9))
+        return _0x41a28d[_0x591ae6(0x19c)]('Envía\x20o\x20responde\x20a\x20una\x20imagen\x20con\x20el\x20mensaje\x20' + _0x19c1c0[_0x591ae6(0x1b1)](_0x57b552, _0x407f56));
+    try {
+        const _0x3373f0 = await _0x3b8435[_0x591ae6(0x1aa)]();
+        if (!_0x3373f0)
+            return _0x41a28d[_0x591ae6(0x19c)](_0x19c1c0[_0x591ae6(0x1a4)]);
+        await _0x126c88[_0x591ae6(0x197)](_0x539f77, _0x3373f0), _0x41a28d[_0x591ae6(0x19c)](_0x19c1c0[_0x591ae6(0x1a3)]);
+    } catch (_0x517c7c) {
+        _0x126c88[_0x591ae6(0x195)][_0x591ae6(0x194)](_0x517c7c), _0x41a28d[_0x591ae6(0x19c)](_0x591ae6(0x1a2) + _0x517c7c[_0x591ae6(0x1af)]);
+    }
+};
+function _0x55ec(_0xdc87b8, _0x223206) {
+    const _0x5ec304 = _0x2336();
+    return _0x55ec = function (_0x4d2899, _0x495f39) {
+        _0x4d2899 = _0x4d2899 - (0x473 + 0x41 * 0x2f + -0xece);
+        let _0x2da2fd = _0x5ec304[_0x4d2899];
+        return _0x2da2fd;
+    }, _0x55ec(_0xdc87b8, _0x223206);
+}
+handler[_0x3faa7c(0x1b0)] = [_0x3faa7c(0x1ae)], handler['tags'] = [_0x3faa7c(0x19e)], handler['command'] = /^setpp?$/i, handler[_0x3faa7c(0x1a0)] = !![];
+export default handler;
