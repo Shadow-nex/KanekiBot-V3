69a396da61c22d80e43d671b90a9e5c769e5b431
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-11-30 19:31:42 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -215,14 +215,7 @@ handler.before = async function (m, { conn, participants, groupMetadata }) {
         { quoted: fkontak }
       )
       
-      await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-/*
-      await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-      await conn.sendMessage(m.chat, {
-        audio: { url: "https://qu.ax/GMQnD.m4a" },
-        mimetype: "audio/mpeg",
-        ptt: true
-      })*/
+     await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
     }
 
     if (chat.welcome &&
