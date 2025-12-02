eaa9b708605f2dad10209eee27643760fdee093b
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-12-02 13:01:07 -0500

    Actualizar game-trivia.js

--- plugins/game-trivia.js
+++ plugins/game-trivia.js
@@ -1,8 +1,8 @@
 import fetch from "node-fetch"
 
 const triviaImages = [
-  '',
-  '',
+  'https://i.pinimg.com/originals/b3/67/d5/b367d513d861de468305c32c6cd22756.jpg',
+  'https://i.pinimg.com/originals/b3/67/d5/b367d513d861de468305c32c6cd22756.jpg',
   'https://i.pinimg.com/originals/b3/67/d5/b367d513d861de468305c32c6cd22756.jpg'
 ]
 
