a270e50abfe1a68e382c81cc8d6fc987562323c0
shadox-xyz <blackoficial2025@gmail.com>
2025-12-02 15:35:18 -0500

    𝒰𝓅𝒻𝒶𝓉𝑒𝓈 𝒮𝒽𝒶𝒹𝑜𝓌.𝓍𝓎𝓏

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -69,10 +69,12 @@ let handler = async (m, { conn, usedPrefix, __dirname, participants }) => {
     for (let tag in tags) {
       let comandos = commands
         .filter(cmd => cmd.tags.includes(tag))
-        .map(cmd => cmd.help.map(e => `*│ ➩ ${usedPrefix}${e}*`).join('\n'))
+        .map(cmd => cmd.help.map(e => `*➩* \`\`\`${usedPrefix}${e}\`\`\``).join('\n'))
         .join('\n')
       if (comandos) {
-        menuTexto += `\n\n*╭──꒰* ${tags[tag]} *꒱𔖲𔖮𔖭*\n${comandos}\n*╰─────────────┈┄╌*\n`
+        menuTexto += `\n\nׅꦿ⿻ ${tags[tag]}
+
+ ۫  𖢷͜੭ ׅ ✎ֹ 𔐼ֹ֪${comandos}\n`
       }
     }
 
