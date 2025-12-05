0c9dea7c6fa2f19fb7385fb7d222cccefae71ba8
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-12-05 10:27:53 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,14 +1,49 @@
-import fetch from "node-fetch"
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn }) => {
-  let r = await fetch("https://akirax-api.vercel.app/nsfw/waifu")
-  let j = await r.json()
+let handler = async (m, { conn, args }) => {
+let mentionedJid = await m.mentionedJid
+let userId = mentionedJid && mentionedJid[0] ? mentionedJid[0] : m.sender
+let totalreg = Object.keys(global.db.data.users).length
+let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
 
-  let img = await fetch(j.url)
-  let buff = await img.buffer()
+let txt = `̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮   ̮
+︶•︶°︶•︶°︶•︶°︶•︶°︶•︶°︶
+> ❀ hola! @${userId.split('@')[0]}, Soy ${botname}, Aquí tienes la lista de comandos.
 
-  await conn.sendMessage(m.chat, { image: buff, caption: `Creator: ${j.creator}` }, { quoted: m })
+╭┈ࠢ͜┅ࠦ͜͜╾݊͜─ؕ͜─ׄ͜─֬͜─֟͜─֫͜─ׄ͜─ؕ͜─݊͜┈ࠦ͜┅ࠡ͜͜┈࠭͜͜۰۰͜۰
+│✦ *Tipo* » ${(conn.user.jid == global.conn.user.jid ? 'Principal' : 'Sub-Bot')}
+│✰ *Usuarios* » ${totalreg.toLocaleString()}
+│⚘ *Versión* » ${vs}
+│ꕥ *Plugins* » ${totalCommands}
+│🜸 *Librería* » ${libreria}
+╰ׅ┈ࠢ͜─ׄ͜─ׄ֟፝͜─ׄ͜─ׄ͜╴ ⋱࣭ ᩴ  ⋮֔   ᩴ ⋰╶͜─ׄ͜─ׄ֟፝͜─ׄ͜─ׄ͜┈ࠢ͜╯ׅ
+
+ׅ`.trim()
+await conn.sendMessage(m.chat, { 
+text: txt,
+contextInfo: {
+mentionedJid: [userId],
+isForwarded: true,
+forwardedNewsletterMessageInfo: {
+newsletterJid: channelRD.id,
+serverMessageId: '',
+newsletterName: channelRD.name
+},
+externalAdReply: {
+title: botname,
+body: textbot,
+mediaType: 1,
+mediaUrl: redes,
+sourceUrl: redes,
+thumbnail: await (await fetch(banner)).buffer(),
+showAdAttribution: false,
+containsAutoReply: true,
+renderLargerThumbnail: true
+}}}, { quoted: m })
 }
 
+handler.help = ['test']
+handler.tags = ['main']
 handler.command = ['test']
+
 export default handler
