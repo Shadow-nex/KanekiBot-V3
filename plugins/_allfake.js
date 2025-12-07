f2edd27e15c80d47dededfcd1ad439826f2dd045
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-12-07 07:56:18 -0500

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -73,11 +73,11 @@ global.icono = [
 'https://cdn.yupra.my.id/yp/uwx1azzu.jpg',
 'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1763311856541_363781.jpeg'
 ].getRandom()
-/*
+
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
 }
-*/
-global.rcanal = {
+
+global.rcanals = {
   contextInfo: {
     isForwarded: true,
     forwardedNewsletterMessageInfo: {
