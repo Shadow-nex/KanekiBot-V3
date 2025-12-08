66f89a2007848ae4fd026eea3d6385ef3b473a83
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-12-08 10:07:18 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -7,8 +7,8 @@ function pickRandom(list) {
 
 const handler = async (m, { conn }) => {
   const stikerxd = [
-    'https://raw.githubusercontent.com/El-brayan502/dat1/main/uploads/e2ac50-1762063291460.webp',
-    'https://raw.githubusercontent.com/El-brayan502/dat1/main/uploads/5ca46a-1762063299079.webp',
+    'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1765206411174_96183.webp',
+    'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1765206396554_438808.webp',
     'https://raw.githubusercontent.com/El-brayan502/dat1/main/uploads/caf9ab-1762063306221.webp',
     'https://raw.githubusercontent.com/El-brayan502/dat4/main/uploads/a96e48-1762063311159.webp',
      'https://raw.githubusercontent.com/El-brayan502/dat1/main/uploads/ea45de-1762063545565.webp',
