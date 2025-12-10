ed5f1e4a10005e7a27c0e0e0dcfe661bdeb450e8
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-12-10 15:04:17 -0500

    Actualizar downloads-modapk.js

--- plugins/downloads-modapk.js
+++ plugins/downloads-modapk.js
@@ -7,7 +7,7 @@ await m.react('🕒')
 let searchA = await search(text)
 let data5 = await download(searchA[0].id)
 let txt = `*乂  APTOIDE - DESCARGAS 乂*\n\n`
-txt += `> ≡ *\|Nombre\`* : ${data5.name}\n`
+txt += `> ≡ *\`Nombre\`* : ${data5.name}\n`
 txt += `> ≡ *\`Package\`* : ${data5.package}\n`
 txt += `> ≡ *\`Update\`* : ${data5.lastup}\n`
 txt += `> ≡ *\`Peso\`* :  ${data5.size}`
