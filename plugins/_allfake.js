d7c6b24edc75bbfcc166fc2aba718d0a63da3c84
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-12-11 17:55:36 -0500

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -65,8 +65,8 @@ global.saludo = hour
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
 global.icono = [
-'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1765493573630_569729.jpeg',
-'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1765493513276_511755.jpeg'
+'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1765493712024_73595.jpeg',
+'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1765493701685_514638.jpeg'
 ].getRandom()
 
 /*global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
