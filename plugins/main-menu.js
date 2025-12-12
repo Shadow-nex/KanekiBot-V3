0127f3ec8fdce9aaa896807cb9322d981ad06e31
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-12-12 11:58:53 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -139,7 +139,7 @@ contextInfo: {
    mediaType: 1,
    mediaUrl: redes,
    sourceUrl: redes,
-   thumbnail: await (await fetch('https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1765134859629_740404.jpeg')).buffer(),
+   thumbnail: await (await fetch('https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1765558675009_50489.jpeg')).buffer(),
    showAdAttribution: false,
    containsAutoReply: true,
    renderLargerThumbnail: true
